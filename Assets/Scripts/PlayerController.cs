aa9ad083aca20d35537905024bccf9db4bf8dd1f
almidds <almidds@hotmail.com>
2023-07-07 22:04:25 +0100

    Add activation cooldown

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,21 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour{
 
     [SerializeField]
-    private float moveSpeed = 5f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private float moveSpeed = 5f, activationCooldownMax = 3f;
+
+    private float activationCooldown;
+    private bool canActivate = true;
+
+    void Start(){
+        activationCooldown = activationCooldownMax;
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
+    void Update(){
         Vector3 move = Vector3.Normalize(new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0)) * moveSpeed;
         transform.position += move * Time.deltaTime;
+
+        if(Input.GetKeyDown(KeyCode.Space) && canActivate){
+            ActivateEnemies();
+            canActivate = false;
+        }
+
+        if(!canActivate){
+            activationCooldown -= Time.deltaTime;
+            if(activationCooldown <= 0f){
+                canActivate = true;
+                activationCooldown = activationCooldownMax;
+                Debug.Log("Cooldown is ready");
+            }
+        }
+
+    }
+
+    void ActivateEnemies(){
+        Debug.Log("I have activated");
     }
 }
