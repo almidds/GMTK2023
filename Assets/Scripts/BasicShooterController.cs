0e8cd18834c775a0abb5165c43dc3c3c7662460c
almidds <almidds@hotmail.com>
2023-07-08 12:01:11 +0100

    Add basic movement

--- Assets/Scripts/BasicShooterController.cs
+++ Assets/Scripts/BasicShooterController.cs
@@ -7,12 +7,23 @@ public class BasicShooterController : Enemy{
     private Transform shootPoint;
     private Transform player;
 
+    private float angle;
+    private float startAngle;
+
+    [SerializeField]
+    private float moveSpeed, angularSpeed;
+    private float randomMoveSpeed, randomAngularSpeed;
+    private float rotationDirection;
+
+    private float moveTime;
+    private bool isRotating;
+
     void Start(){
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     void Update(){
-        
+        Move();
     }
 
     public override void Shoot(){
@@ -22,5 +33,32 @@ public class BasicShooterController : Enemy{
             bullet.GetComponent<ProjectileController>().direction = Vector3.Normalize(direction);
             bullet.GetComponent<ProjectileController>().parentName = gameObject.name;
         }
+    }
+
+    private void Move(){
+        moveTime -= Time.deltaTime;
+        if(moveTime <= 0){
+            isRotating = !isRotating;
+            moveTime = Random.Range(2f, 5f);
+            randomMoveSpeed = moveSpeed * Random.Range(0.9f, 1.1f);
+            randomAngularSpeed = angularSpeed * Random.Range(0.9f, 1.1f);
+            rotationDirection = Mathf.Sign(Random.Range(-1f, 1f));
+        }
+        if(isRotating){
+            orbitPlayer();
+        }
+        else{
+            moveTowardsPlayer();
+        }
+    }
+
+    private void orbitPlayer(){
+        Quaternion rotation = transform.rotation;
+        transform.RotateAround(player.position, Vector3.forward, rotationDirection * angularSpeed * Time.deltaTime);
+        transform.rotation = rotation;
+    }
+
+    private void moveTowardsPlayer(){
+        transform.position = Vector3.MoveTowards(transform.position, player.position, Time.deltaTime * moveSpeed);
     }
 }
