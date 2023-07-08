c154b877bb4e4a9d13f0fe52e2d24241637723cc
almidds <almidds@hotmail.com>
2023-07-08 14:24:03 +0100

    Add font

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour{
+    // Amount of time the game goes on for, how long between waves
+    [SerializeField]
+    private float timer = 300f, timeBetweenWavesMax = 40f, timeBetweenWaves;
+
+    void Start(){
+        
+    }
+
+    void Update(){
+        
+    }
+}
