5dbb83f5fd0918b8daabb387faea562dbb339f7a
almidds <almidds@hotmail.com>
2023-07-09 09:21:29 +0100

    Add Main Menu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour{
+    [SerializeField] private Image black;
+    [SerializeField] private Animator anim;
+    private bool activated = false;
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Return) && !activated){
+            activated = true;
+            StartCoroutine(Fading());
+        }
+    }
+
+    IEnumerator Fading(){
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(()=>black.color.a==1);
+        SceneManager.LoadScene("SampleScene");
+    }
+}
